d61c8441c140bf07caf19d7e100961588c0e1be8
SL0TR <mohaiminislam94@gmail.com>
2021-01-21 01:22:40 +0600

    chores: - migrate to and configure parcel for the ts file compile and builds - add new htmlmarkup for the project - load the template html on the app container - create autobind decorator - add private methods on the projectTemp class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('test')
+import { AutoBind } from "./decorators";
+
+class ProjectTemplate {
+  templateElem: HTMLTemplateElement;
+  hostElem: HTMLDivElement;
+  appContainerElem: HTMLDivElement;
+  formElem: HTMLFormElement
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElem = document.getElementById('project-template')! as HTMLTemplateElement;
+    this.hostElem = document.getElementById("app")! as HTMLDivElement;
+
+    const tempNode = document.importNode(this.templateElem.content, true);
+    this.appContainerElem = tempNode.firstElementChild as HTMLDivElement;
+    this.formElem = this.appContainerElem.querySelector('#project-form')! as HTMLFormElement;
+
+    this.titleInputElement = this.appContainerElem.querySelector('#title')! as  HTMLInputElement;
+    this.descriptionInputElement = this.appContainerElem.querySelector('#description')! as  HTMLInputElement;
+    this.peopleInputElement = this.appContainerElem.querySelector('#people')! as  HTMLInputElement;
+
+    this.configure()
+    this.attach() 
+  }
+
+
+  @AutoBind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputElement.value)
+  }
+  
+  private configure() {
+    this.formElem.addEventListener('submit', this.submitHandler)
+  }
+
+  private attach() {
+    this.hostElem.insertAdjacentElement('afterbegin', this.appContainerElem);
+  }
+}
+
+const projTemp = new ProjectTemplate()
+
+console.log(projTemp)
